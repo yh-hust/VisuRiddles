b6cce5029143bb5e49f09baf40b6db1f7c32854e
yh-hust <haoyan@hust.edu.cn>
2025-05-26 22:04:47 +0800

    Create inference_deepseekvl.sh

---
+++ scripts/offline/inference_deepseekvl.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+NGPUS_PER_NODE=1
+NNODES=1
+NODE_RANK=0
+MASTER_ADDR=127.0.0.1
+MASTER_PORT=29500
+model_name=deepseek_vl2
+tp=1
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+SAVE_DIR=output/$model_name
+LOG_DIR=$SAVE_DIR/logs/qwen_mpdoc_eval_${TIMESTAMP}.log
+model=/cache/weight/$model_name
+dataset_path='path'
+mkdir -p "$SAVE_DIR"
+mkdir -p "$SAVE_DIR/logs"
+
+DISTRIBUTED_ARGS="--nproc_per_node=$NGPUS_PER_NODE --nnodes=$NNODES --node_rank=$NODE_RANK --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT"
+SCRIPT_ARGS="--model $model --save_dir $SAVE_DIR --dataset_path $dataset_path --tp $tp"
+echo $DISTRIBUTED_ARGS
+echo $SCRIPT_ARGS
+
+export CUDA_VISIBLE_DEVICES=0
+python evaluate/inference_offline.py $SCRIPT_ARGS 2>&1 | tee -a $LOG_DIR
